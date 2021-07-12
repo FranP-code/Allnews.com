330891a5488cce3f49b566621b7d0c6895bd9820
Francisco Pessano <franpessano1@gmail.com>
2021-07-12 14:19:47 -0300

    Avanzado sobre el backend

--- news_logic.php
+++ news_logic.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(~0);
+ini_set('display_errors', 1);
+
 function check_news() {
     $news = glob('./news/*');
 
@@ -21,9 +24,12 @@ function check_news() {
             echo '<br>';
     
             if (!strstr($news_done, $new)) {
-                $author = know_autor($new); //! HACER FUNCION
-                $create_entry($new, $author); //! HACER FUNCION
-                file_put_contents($news_done, $new . '/n', FILE_APPEND);
+                $page = know_page($new);
+                echo $page;
+                $author = know_author($page, $new);
+                echo $author;
+                $create_entry_in_DB($new, $author); //! HACER FUNCION
+                //file_put_contents($news_done, $new . '/n', FILE_APPEND);
             }
 
             /*if ($new is in $done) {
@@ -32,5 +38,55 @@ function check_news() {
         }
     }
 }
+
+function know_page($new) {
+    switch ($new) {
+        case './news/xataka.html'|| './news/Xataka.html':
+            return 'Xataka';
+            break;
+
+        default:
+            return 0;
+            break;
+    }
+}
+
+function know_author($page, $new) {
+    $content = file_get_contents($new);
+
+    switch ($page) {
+        case 'Xataka':
+            $frist_cut = get_string_between($content, '<a class="article-author-link" ', '</a>');
+            $second_cut = strrchr($frist_cut, '>');
+            return str_replace('>', '', $second_cut);
+            break;
+        
+        default:
+            return 0;
+            break;
+    }
+}
+
+function get_string_between($string, $start, $end){
+    $string = ' ' . $string;
+    $ini = strpos($string, $start);
+    if ($ini == 0) return '';
+    $ini += strlen($start);
+    $len = strpos($string, $end, $ini) - $ini;
+    return substr($string, $ini, $len);
+    //! CREDITS: https://stackoverflow.com/questions/5696412/how-to-get-a-substring-between-two-strings-in-phphttps://stackoverflow.com/questions/5696412/how-to-get-a-substring-between-two-strings-in-php
+}
+
+/* 
+switch ($variable) {
+    case 'value':
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
+*/
 
 ?>
